56960f1dfc091b4a18827aab1c36dcc026108d5c
N4th9 <128613887+N4th9@users.noreply.github.com>
2024-01-08 20:00:40 +0100

    Finalisation du projet en cours => TOUS LES PLANS

---
+++ public/client.js
@@ -0,0 +1,363 @@
+const localhost = "http://localhost:3500"
+
+var MyActualProject = {
+    id: 0,
+    name: "",
+    description:""
+}
+var MyActualPiece = {
+    id: 0,
+    IdProjet: 0,
+    Piecename: "",
+    Piecedescription:""
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const BtnBurger = document.getElementById("sidebar")
+    const BtnPiece = document.getElementById("sidePiece")
+    const ShowPiece = document.getElementById("ShowPiece")
+    const ButtonProject = document.getElementById("btnproject")
+    const ListProject = document.getElementById("projectList")
+    const Project = document.getElementById("Project")
+    const ProjectTitle = document.getElementById("ProjectTitle")
+    const DescriptionTitle = document.getElementById("DescriptionTitle")
+    const deleteproject = document.getElementById("deleteproject")
+    const ButtonPiece = document.getElementById("btnpiece")
+    const PieceTitle = document.getElementById("PieceTitle")
+    const DescriptionPieceTitle = document.getElementById("DescriptionPieceTitle")
+    const Pieces = document.getElementById("listpiece")
+    const deletepiece = document.getElementById("deletepiece")
+    const FormUploads = document.getElementById("FormUploads")
+    const FormUploadsPlans = document.getElementById("FormUploadsPlans")
+    const FormAllPlans = document.getElementById("BtnAllPlans")
+    const ShArchives = document.getElementById("ShowArchives")
+    const Archives = document.getElementById("Archives")
+    const ListArchives = document.getElementById("listArchives")
+    const buttonArchives = document.getElementById("buttonArchives")
+    const HiArchives = document.getElementById("HiddenArchives")
+
+    //******************Keyup of Event************************
+    ProjectTitle.addEventListener('keyup', UpdateProject)
+    DescriptionTitle.addEventListener('keyup', UpdateProject)
+    PieceTitle.addEventListener('keyup', UpdatePiece)
+    DescriptionPieceTitle.addEventListener('keyup', UpdatePiece)
+    //******************Click of Event************************
+    BtnBurger.addEventListener("click", Translated)
+    BtnPiece.addEventListener("click", TranslatedPiece)
+    ButtonProject.addEventListener("click", CreateProject)
+    deleteproject.addEventListener("click", DeleteProject)
+    ButtonPiece.addEventListener("click", CreatePiece)
+    deletepiece.addEventListener("click", DeletePiece)
+    FormUploads.addEventListener("submit", submitForm)
+    FormUploadsPlans.addEventListener("submit", submitPlan)
+    FormAllPlans.addEventListener("click", submitAllPlans)
+    ShArchives.addEventListener("click", ShowArchives)
+    HiArchives.addEventListener("click", HiddenArchives)
+
+    DisplayProject()
+    let HArchives = document.getElementById("HiddenArchives")
+    HArchives.style.display = "none"
+    Archives.style.display = "none"
+
+    function CreateProject() {
+        fetch(localhost + "/api/Projet", {
+            method: 'POST'
+        })
+            .then(res => console.log(res))
+            .then(DisplayProject)
+    }
+
+    function CreatePiece() {
+        fetch(localhost + "/api/Piece/" + MyActualProject.id + "/piece", {
+            method: "POST"
+        })
+            .then(res => console.log(res))
+            .then(DisplayPieces)
+        ShowPiece.style.display = ""
+    }
+
+    function DisplayProject() {
+        ShowPiece.style.display = ""
+        fetch(localhost + "/api/Projets", {
+            method: "GET"
+        })
+            .then(dataProject => dataProject.json())
+            .then(function (dataProject) {
+                ListProject.innerText = ""
+                for (let i = 0; i < dataProject.length; i++) {
+                    let p = document.createElement("p")
+                    p.innerText = dataProject[i].Nom
+                    p.addEventListener("click", function () {
+                        LoadDataProject(dataProject, i)
+                        DisplayPieces()
+                        ShowPiece.style.display = ""
+                    })
+                    let a = document.createElement("button")
+                    let imga = document.createElement("img")
+                    imga.src = "/img/archives.png";
+                    imga.style.height = '20px';
+                    a.style.marginRight = '10px'
+                    a.style.marginLeft = '10px'
+                    a.append(imga)
+
+                    let b = document.createElement("button")
+                    let imgb = document.createElement("img")
+                    imgb.src = "/img/trash.png";
+                    imgb.style.height = '20px';
+                    imgb.style.padding = '1px';
+                    b.append(imgb)
+
+                    ListProject.append(p)
+                    p.append(a)
+                    p.append(b)
+
+                    imga.addEventListener("click", function () {
+                        fetch(localhost + "/api/ArchiveProject/" + MyActualProject.id, {
+                            method: "PUT"
+                        })
+                            .then(DisplayProject)
+                            .then(DisplayArchives)
+                    })
+                }
+             })
+    }
+
+    function DisplayPieces() {
+        ShowPiece.style.display = ""
+        fetch(localhost + "/api/PiecesOfProjets/" + MyActualProject.id, {
+            method: "GET"
+        })
+            .then(dataPiece => dataPiece.json())
+            .then(function (dataPiece) {
+                console.log(MyActualProject)
+                Pieces.innerText = ""
+                for (let i = 0; i < dataPiece.length; i++) {
+                    let p = document.createElement("p");
+                    p.innerText = dataPiece[i].Nom
+                    p.addEventListener("click", function () {
+                        LoadDataPiece(dataPiece, i)
+                        ShowPiece.style.display = ""
+                    })
+                    Pieces.append(p);
+                }
+            })
+    }
+
+    function UpdateProject() {
+        MyActualProject.name = ProjectTitle.innerText
+        MyActualProject.description = DescriptionTitle.innerText
+
+        fetch(localhost + "/api/UpdateProjet/" + MyActualProject.id, {
+            method: 'PUT',
+            body: JSON.stringify(MyActualProject),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(DisplayProject)
+    }
+
+    function UpdatePiece() {
+        MyActualPiece.Piecename = PieceTitle.innerText
+        MyActualPiece.Piecedescription = DescriptionPieceTitle.innerText
+
+        fetch(localhost + "/api/UpdatePiece/" + MyActualPiece.id, {
+            method: 'PUT',
+            body: JSON.stringify(MyActualPiece),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(DisplayPieces)
+    }
+
+    function DeleteProject() {
+        fetch(localhost + "/api/DeleteProject/" + MyActualProject.id, {
+            method: 'Delete'
+        })
+            .then(DisplayProject)
+    }
+
+    function DeletePiece() {
+        fetch(localhost + "/api/DeletePiece/" + MyActualPiece.id, {
+            method: 'Delete'
+        })
+            .then(DisplayPieces)
+    }
+
+    function LoadDataProject(data, i) {
+        MyActualProject.id = data[i].ID
+        MyActualProject.name = data[i].Nom
+        MyActualProject.description = data[i].Description
+
+        ProjectTitle.innerText = MyActualProject.name
+        DescriptionTitle.innerText = MyActualProject.description
+    }
+
+    function LoadDataPiece(data, i,) {
+        MyActualPiece.id = data[i].ID
+        MyActualPiece.PieceProjid = data[i].IdProjet
+        MyActualPiece.Piecename = data[i].Nom
+        MyActualPiece.Piecedescription = data[i].Description
+        console.log(MyActualPiece)
+
+        PieceTitle.innerText = MyActualPiece.Piecename
+        DescriptionPieceTitle.innerText = MyActualPiece.Piecedescription
+
+        //***************Permet de mettre à jour l'affichage des images***********
+        document.querySelector('#Pictures').innerText=""
+        fetch(localhost + "/uploads/" + MyActualPiece.id,{
+            method:"GET"
+        })
+            .then(data => data.json())
+            .then(function (data) {
+                DisplayImages(data)
+            })
+        //***************Permet de mettre à jour l'affichage des plans***********
+        document.querySelector('#Plans').innerText=""
+        fetch(localhost + "/uploads/" + MyActualPiece.id,{
+            method:"GET"
+        })
+            .then(data => data.json())
+            .then(function (data) {
+                DisplayPlans(data)
+            })
+
+        DisplayImages()
+        DisplayPlans()
+    }
+    function submitForm(e) {
+        e.preventDefault();
+
+        fetch(localhost + "/uploads_img/" + MyActualPiece.id, {
+            method: 'POST',
+            body: new FormData(document.getElementById("FormUploads"))
+        })
+            .then(res => res.json())
+            .then(DisplayImages)
+            .catch((err) => ("Error occured", err));
+    }
+    function DisplayImages(data) {
+        fetch(localhost + "/uploads/" + MyActualPiece.id, {
+            method: "GET"
+        }).then(data=> data.json())
+        for (let i = 0; i < data.length; i++) {
+            let img = document.createElement('img')
+            img.src = "uploads/" + data[i].Nom
+            let p = document.querySelector('#Pictures')
+            p.append(img)
+        }
+    }
+    function submitPlan(e){
+        e.preventDefault();
+
+        fetch(localhost + "/uploads_plans/" + MyActualPiece.id, {
+            method: 'POST',
+            body: new FormData(document.getElementById("FormUploadsPlans"))
+        })
+            .then(res => res.json())
+            .then(DisplayPlans)
+            .catch((err) => ("Error " + err));
+    }
+    function DisplayPlans() {
+        fetch(localhost + "/uploads_SendPlan/" + MyActualPiece.id,{
+            method:"GET"
+        })
+            .then(data => data.json())
+            .then(function (data){
+                for(let i = 0; i < data.length; i++){
+                    let img = document.createElement('img')
+                    img.src = "uploads/" + data[i].Nom
+                    let div = document.querySelector('#Plans')
+                    div.append(img)
+                }
+            })
+    }
+    function submitAllPlans(){
+        fetch(localhost + '/submitAllPlans/' + MyActualProject.id,{
+            method:"GET"
+        })
+            .then(data => data.json())
+            .then(function (data){
+                ShowPiece.style.display = "none"
+                let div = document.querySelector('#Plans')
+                for(let i = 0; i < data.length; i++){
+                    let img = document.createElement('img')
+                    img.maxWidth = "500px"
+                    img.height = "auto"
+                    img.src = "uploads/"
+                    div.append(img)
+                }
+            })
+    }
+    function ShowArchives() {
+        document.getElementById("Project").style.display = "none"
+        ShArchives.style.display = "none"
+        HArchives.style.display = ""
+        Archives.style.display = ""
+        DisplayArchives()
+    }
+    function DisplayArchives() {
+        ListArchives.innerText = "";
+        buttonArchives.innerText = "";
+
+        fetch(localhost + "/api/ProjetsArchives", {
+            method: "GET"
+        })
+            .then(dataProject => dataProject.json())
+            .then(function (dataProject) {
+                for (let i = 0; i < dataProject.length; i++) {
+                    let c = document.createElement("p");
+                    c.innerText = dataProject[i].Nom;
+                    c.classList.add("pNomArchives");
+
+                    let imgc = document.createElement("img");
+                    imgc.src = "/img/archives.png";
+                    imgc.style.height = '30px';
+
+                    let imgd = document.createElement("img");
+                    imgd.src = "/img/trash.png";
+                    imgd.style.height = '30px';
+
+                    ListArchives.append(c);
+
+                    let imgContainer = document.createElement("div");
+                    imgContainer.style.flexDirection = "column";
+                    imgContainer.style.marginBottom = "50px";
+
+                    imgContainer.append(imgc);
+                    imgContainer.append(imgd);
+
+                    buttonArchives.append(imgContainer);
+
+                    imgc.addEventListener("click", function () {
+                        fetch(localhost + "/api/UnArchiveProject/" + dataProject[i].ID, {
+                            method: "PUT"
+                        })
+                            .then(DisplayArchives)
+                            .then(DisplayProject)
+                    })
+
+                    imgd.addEventListener("click", function () {
+                        fetch(localhost + "/api/DeleteProject/" + dataProject[i].ID, {
+                            method: "DELETE"
+                        })
+                            .then(DisplayArchives)
+                    })
+                }
+            });
+    }
+
+    function HiddenArchives(){
+        document.getElementById("Project").style.display = ""
+        ShArchives.style.display = ""
+        HArchives.style.display = "none"
+        Archives.style.display = "none"
+    }
+    function Translated() {
+        Project.classList.toggle("translated")
+    }
+    function TranslatedPiece() {
+        ShowPiece.classList.toggle("translatedPiece")
+    }
+})
---
+++ server.js
@@ -0,0 +1,225 @@
+const express = require('express')
+const sqlite3 = require('sqlite3');
+const multer = require ('multer')
+
+const app = express()
+app.use(express.static("public"))
+app.listen(3500, () => console.log("Server ready !"))
+
+const upload = multer({ dest:"uploads/"});
+app.use("/uploads", express.static('uploads'))
+
+//*******line to update my db************
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//*********************************Check if database ok************************
+const db = new sqlite3.Database('./ARCHITECT.db', (err) => {
+    if(err){
+        console.log("Cannot open database")
+        console.log(err.message)
+        throw err;
+    }else{
+        console.log("database open")
+        console.log("address http://localhost:3500")
+    }
+})
+//**************************Display all projects*******************************
+app.get("/api/Projets", (req, res)=>{
+    db.all("SELECT * FROM Projets WHERE Supprime = 0;", (err, row)=>{
+        if (err){
+            return console.log(err.message)
+        }else{
+            console.log(row)
+            res.send(row)
+        }
+    })
+})
+//**************************INSERT NEW PROJECT*********************************
+app.post("/api/Projet", (req, res) => {
+    db.run("INSERT INTO Projets (Nom, Description, Supprime) values ('Nouveau projet','Description', 0);", function (err){
+        if (err){
+            return console.log(err.message)
+        }
+        res.send()
+        console.log("[Projet ajouté]")
+
+    })
+})
+//**************************INSERT NEW PIECE*********************************
+app.post("/api/Piece/:id/piece", (req, res) => {
+db.run("INSERT INTO Pieces (IdProjet, Nom, Description) values(?,'Nouvelle pièce','Description');",[req.params.id],function (err){
+        if (err){
+            return console.log(err.message)
+        }
+        res.json({message : "Is fully added"})
+        console.log("[Piece ajouté]")
+    })
+})
+//***************************UPDATE NAME OF PROJECT***************************
+app.put("/api/UpdateProjet/:id", (req, res)=>{
+    db.run("UPDATE Projets SET Nom = ?, Description = ? WHERE ID = ?;",[req.body.name,req.body.description,req.params.id], (err)=>{
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            res.send(req.body)
+            console.log("[Projet modifié]")
+        }
+    } )
+})
+//***************************UPDATE NAME OF PIECE***************************
+app.put("/api/UpdatePiece/:id", (req, res)=>{
+    db.run("UPDATE Pieces SET Nom = ?, Description = ? WHERE ID = ?;",[req.body.Piecename,req.body.Piecedescription,req.params.id],(err)=>{
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            res.send(req.body)
+            console.log("[Pièce modifiée]")
+        }
+    })
+})
+//***************************DELETE A PROJECT********************************
+app.delete("/api/DeleteProject/:id",(req,res)=>{
+    db.run("DELETE FROM Projets WHERE ID = "+ req.params.id +";", (err)=> {
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            console.log("[Projet effacé]")
+            res.json("[PROJET EFFACE]")
+        }
+    })
+})
+//***************************DELETE A PIECE********************************
+app.delete("/api/DeletePiece/:id",(req,res)=>{
+    db.run("DELETE FROM Pieces WHERE ID = "+ req.params.id +";", (err)=> {
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            console.log("[piece effacé]")
+            res.json("[PIECE EFFACE]")
+        }
+    })
+})
+//**************************UPLOAD PICTURES TO FOLDER UPLOADS******************
+app.post('/uploads_img/:id', upload.single('file'), uploadpictures)
+    function uploadpictures(req, res){
+        console.log(req.file)
+        db.run("INSERT INTO Images (Nom, PieceID) values(?, ?);",[req.file.filename,req.params.id],(err)=>{
+            if(err){
+                return console.log(err.message)
+            }
+            else {
+                console.log("[image bien ajouter]")
+            }
+        })
+    }
+//**************************SEND PICTURES TO NAVIGATOR***********************
+app.get('/uploads/:id',(req,res)=>{
+    db.all("SELECT Images.ID,Images.Nom FROM Images JOIN Pieces ON Pieces.ID = Images.PieceID WHERE Pieces.ID = ?",[req.params.id],(err,row)=>{
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            console.log("image bien envoyer")
+            res.send(row)
+        }
+    })
+})
+//**************************UPLOAD PLANS TO FOLDER UPLOADS******************
+app.post('/uploads_plans/:id', upload.single('fileplan'), uploadplans)
+function uploadplans(req, res){
+    console.log(req.file)
+    db.run("INSERT INTO Plans (Nom, PieceIDPlan) values(?, ?);",[req.file.filename,req.params.id],(err)=>{
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            console.log("[plan bien ajouter]")
+        }
+    })
+}
+//**************************SEND PLANS TO NAVIGATOR***********************
+app.get('/uploads_SendPlan/:id',(req,res)=>{
+    db.all("SELECT Plans.ID,Plans.Nom FROM Plans JOIN Pieces ON Pieces.ID = Plans.PieceIDPlan WHERE Pieces.ID = ?",[req.params.id],(err,row)=>{
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            console.log("plan bien envoyer")
+            res.send(row)
+        }
+    })
+
+})
+//******************Tous les plans*************************
+app.get('/uploads_AllPlan',(req,res)=>{
+    db.all("SELECT * FROM Plans;",(err,row)=>{
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            console.log("plan bien envoyer")
+            res.send(row)
+        }
+    })
+
+})
+//*****************Project in archives***********************
+app.put("/api/ArchiveProject/:id",(req,res)=>{
+    db.run("UPDATE Projets SET Supprime = 1 WHERE ID = ?;",[req.params.id],(err)=>{
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            res.send(req.body)
+            console.log("[Projet archivé]")
+        }
+    })
+})
+app.get("/api/ProjetsArchives", (req, res)=>{
+    db.all("SELECT * FROM Projets WHERE Supprime = 1;", (err, row)=>{
+        if (err){
+            return console.log(err.message)
+        }else{
+            console.log(row)
+            res.send(row)
+        }
+    })
+})
+app.put("/api/UnArchiveProject/:id",(req,res)=>{
+    db.run("UPDATE Projets SET Supprime = 0 WHERE ID = ?;",[req.params.id],(err)=>{
+        if(err){
+            return console.log(err.message)
+        }
+        else {
+            res.send(req.body)
+            console.log("[Projet désarchivé]")
+        }
+    })
+})
+//**************************SEND DATA TO NAVIGATOR AT LOCALHOST:3500/api/Projects*********************
+app.get("/api/Projets", (req, res)=>{
+    db.all("SELECT * FROM Projets;", (err, row)=>{
+        if (err){
+            return console.log(err.message)
+        }else{
+            console.log(row)
+            res.send(row)
+        }
+    })
+})
+app.get("/api/PiecesOfProjets/:id", (req, res) => {
+    db.all("SELECT Pieces.ID,IdProjet,Pieces.Nom,Pieces.Description FROM Pieces JOIN Projets ON Projets.ID = IdProjet WHERE IdProjet = ? ;",[req.params.id], (err,rowPiece)=> {
+        if(err){
+            return console.log(err.message)
+        }
+        else{
+            console.log(rowPiece)
+            res.send(rowPiece)
+        }
+    })
+})
